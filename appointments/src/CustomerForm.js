c60ba7ad169151f0a13cb164835fea3e1ff99532
Maximo Calero Sanchez(devmcalero) <maximo.calero@devmcalero.onmicrosoft.com>
2020-10-02 06:55:03 +0200

    exercices testing test input fields

--- appointments/src/CustomerForm.js
+++ appointments/src/CustomerForm.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 
-export const CustomerForm = ({ firstName, onSubmit }) => {
-  const [customer, setCustomer] = useState({ firstName });
+export const CustomerForm = ({ firstName, lastName, onSubmit }) => {
+  const [customer, setCustomer] = useState({ firstName, lastName });
 
   const handleChangeFirstName = ({ target }) =>
     setCustomer(customer => ({
       ...customer,
       firstName: target.value
+    }));
+
+  const handleChangeLastName = ({ target }) =>
+    setCustomer(customer => ({
+      ...customer,
+      lastName: target.value
     }));
 
   return (
@@ -19,6 +25,14 @@ export const CustomerForm = ({ firstName, onSubmit }) => {
         value={firstName}
         onChange={handleChangeFirstName}
       />
+      <label htmlFor="lastName">Last name</label>
+      <input
+        type="text"
+        name="lastName"
+        id='lastName'
+        value={lastName}
+        onChange={handleChangeLastName}
+      />      
     </form>
   );
 };
